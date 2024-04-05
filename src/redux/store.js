ef9824f301872f245f81c2d4d391d343d72c79c5
ViktoriiaYakaba <yakaba@bk.ru>
2024-04-05 17:24:35 +0200

    add task

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './contacts/slice';
+import { authReducer } from './auth/slice';
+import { filtersReducer } from './filters/slice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
