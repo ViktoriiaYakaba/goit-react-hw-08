9407595f9ff43197a7e66b04dbf14a6d8e5a0ed7
ViktoriiaYakaba <yakaba@bk.ru>
2024-04-05 20:22:06 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import  Layout  from './Layout';
-import { PrivateRoute } from './UserMenu/PrivateRoute';
-import { RestrictedRoute } from './UserMenu/RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 import { refreshUser } from '../redux/auth/operations';
 import {useAuth} from '../hooks'
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './components/UserMenu/App.jsx';
+import App from './components/App.jsx';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import 'modern-normalize';
